9d51e1938772f354f975fb0b1b321a2389d1b265
Maksim <mazhur04@gmail.com>
2023-07-10 17:15:58 +0700

    Added string format

--- app/src/main/java/com/example/shiftsummer2023/screen/CharacterAdapter.kt
+++ app/src/main/java/com/example/shiftsummer2023/screen/CharacterAdapter.kt
@@ -1,21 +1,19 @@
 package com.example.shiftsummer2023.screen
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.shiftsummer2023.R
-import com.example.shiftsummer2023.data.models.CharacterModel
 import com.example.shiftsummer2023.databinding.CharacterItemBinding
 import com.example.shiftsummer2023.domain.models.Character
+import com.example.shiftsummer2023.formatCharacterGender
 
 class CharacterAdapter : RecyclerView.Adapter<CharacterAdapter.CharacterViewHolder>() {
 
-    class CharacterViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        private val binding = CharacterItemBinding.bind(view)
+    class CharacterViewHolder(private val binding: CharacterItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
         fun bind(character: Character) = with(binding) { // Потом изменю
             name.text = character.name
-            gender.text = character.gender.name
+            gender.text = formatCharacterGender(itemView.context, gender = character.gender)
         }
     }
 
@@ -26,9 +24,9 @@ class CharacterAdapter : RecyclerView.Adapter<CharacterAdapter.CharacterViewHold
         }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CharacterViewHolder {
-        val view =
-            LayoutInflater.from(parent.context).inflate(R.layout.character_item, parent, false)
-        return CharacterViewHolder(view)
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = CharacterItemBinding.inflate(inflater, parent, false)
+        return CharacterViewHolder(binding)
     }
 
     override fun getItemCount(): Int {
