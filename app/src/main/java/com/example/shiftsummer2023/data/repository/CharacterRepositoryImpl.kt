f4d9947039388683071257cd90fa6ec36052b3ca
Maksim <mazhur04@gmail.com>
2023-07-13 18:58:21 +0700

    Added getting one character

--- app/src/main/java/com/example/shiftsummer2023/data/repository/CharacterRepositoryImpl.kt
+++ app/src/main/java/com/example/shiftsummer2023/data/repository/CharacterRepositoryImpl.kt
@@ -1,19 +1,16 @@
 package com.example.shiftsummer2023.data.repository
 
+import com.example.shiftsummer2023.data.mappers.toCharacter
 import com.example.shiftsummer2023.data.mappers.toCharacters
 import com.example.shiftsummer2023.data.remote.RickAndMortyApi
+import com.example.shiftsummer2023.domain.models.Character
 import com.example.shiftsummer2023.domain.models.Characters
 import com.example.shiftsummer2023.domain.repository.CharacterRepository
 
 class CharacterRepositoryImpl(
     private val rickAndMortyApi: RickAndMortyApi
 ) : CharacterRepository {
-
-    override suspend fun getFirstPage(): Characters {
-        return rickAndMortyApi.getFirstPage().toCharacters()
-    }
-
-    override suspend fun getPageByUrl(url: String): Characters {
-        return rickAndMortyApi.getPageByUrl(url).toCharacters()
+    override suspend fun getCharacterById(id: Int): Character {
+        return rickAndMortyApi.getCharacterById(id = id).toCharacter()
     }
 }
--- app/src/main/java/com/example/shiftsummer2023/presentation/CharacterListViewModel.kt
+++ app/src/main/java/com/example/shiftsummer2023/presentation/CharacterListViewModel.kt
@@ -1,7 +1,5 @@
 package com.example.shiftsummer2023.presentation
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.Pager
@@ -10,17 +8,14 @@ import androidx.paging.cachedIn
 import androidx.paging.map
 import com.example.shiftsummer2023.data.local.entity.CharacterEntity
 import com.example.shiftsummer2023.data.mappers.toCharacter
-import com.example.shiftsummer2023.domain.usecase.GetFirstPageUseCase
-import com.example.shiftsummer2023.domain.usecase.GetPageByUrlUseCase
+import com.example.shiftsummer2023.domain.usecase.GetCharacterByIdUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class CharacterListViewModel @Inject constructor(
-    private val getFirstPageUseCase: GetFirstPageUseCase,
-    private val getPageByUrlUseCase: GetPageByUrlUseCase,
+    private val getCharacterByIdUseCase: GetCharacterByIdUseCase,
     pager: Pager<Int, CharacterEntity>
 ) : ViewModel() {
 
