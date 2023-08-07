1d498fe137a70a45f2477e163ba9472caeb9ff6c
Maksim <mazhur04@gmail.com>
2023-08-07 21:23:33 +0700

    Changed colors

--- app/src/main/java/com/example/shiftsummer2023/presentation/character_list/CharacterListState.kt
+++
@@ -1,8 +0,0 @@
-package com.example.shiftsummer2023.presentation.character_list
-
-import com.example.shiftsummer2023.domain.model.Character
-
-sealed interface CharacterListState {
-    object Initial : CharacterListState
-    data class Content(val items: List<Character>) : CharacterListState
-}
